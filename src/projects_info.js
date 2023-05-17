ff6b1b0932eda7acd61eb7110e60c5fb0d924388
Montinyek <xsrokhan@gmail.com>
2023-05-16 22:56:08 -0700

    New project

--- src/projects_info.js
+++ src/projects_info.js
@@ -1,9 +1,9 @@
 
 import jokes from './jokes.png'
-import tenzies from './tenzies.png'
 import weather from './weather.png'
 import cart from './shopping-cart.png'
 import budget from './budget-app.png'
+import dsadepot from './dsa-depot.png'
 import { FaNodeJs } from 'react-icons/fa'
 import { FaCss3 } from 'react-icons/fa'
 import { FaJs } from 'react-icons/fa'
@@ -20,23 +20,6 @@ export const projects = [
       stack: [<FaJs />, <FaCss3 />, <FaNodeJs />, <SiMongodb />],
       code: ['Code', 'https://github.com/Montinyek/Joke_depo'],
       demo: ['Demo', 'https://js-huk19u.stackblitz.io'],
-      id: 0
-   },
-   {
-      description: 'A classic dice game made with React. Keep rolling while selecting matching numbers and see how long it takes you to win! (My record is 7.25).',
-      image: tenzies,
-      stack: [<FaReact />, <FaCss3 />],
-      code: ['Code', 'https://github.com/Montinyek/Tenzies_game'],
-      demo: ['Demo', 'https://react-l2lm55.stackblitz.io'],
-      id: 0
-   },
-   {
-      description: "This fully responsive weather app features a unique design and gradient backgrounds that change according to the current weather.",
-      image: weather,
-      stack: [<FaReact />, <FaCss3 />],
-      code: ['Code', 'https://github.com/Montinyek/Weather_app'],
-      demo: ['Demo', 'https://react-jxdthk.stackblitz.io'],
-      id: 1
    },
    {
       description: 'A simple React app demonstrating the basic functionality of a modern shopping cart, made using Context API.',
@@ -44,7 +27,20 @@ export const projects = [
       stack: [<FaReact />, <FaCss3 />],
       code: ['Code', 'https://github.com/Montinyek/Shopping_cart'],
       demo: ['Demo', 'https://montinyek.github.io/Shopping_cart/'],
-      id: 2
+   },
+   {
+      description: "This fully responsive weather app features a unique design and gradient backgrounds that change according to the current weather.",
+      image: weather,
+      stack: [<FaReact />, <FaCss3 />],
+      code: ['Code', 'https://github.com/Montinyek/Weather_app'],
+      demo: ['Demo', 'https://react-jxdthk.stackblitz.io'],
+   },
+   {
+      description: 'A digital library, serving as a personal collection of Data Structures and Algorithms problems and solutions, searchable by title and tags.',
+      image: dsadepot,
+      stack: [<FaReact />, <FaCss3 />],
+      code: ['Code', 'https://github.com/Montinyek/DSA_Depot_client'],
+      demo: ['Demo', 'https://www.dsadepot.com/'],
    },
    {
       description: 'This app uses localStorage to keep track of your expenses in custom categories and provides a clear picture of your budget.',
@@ -52,6 +48,5 @@ export const projects = [
       stack: [<FaReact />, <FaBootstrap />],
       code: ['Code', 'https://github.com/Montinyek/Budget_app'],
       demo: ['Demo', 'https://montinyek.github.io/Budget_app/'],
-      id: 3
    }
 ]
